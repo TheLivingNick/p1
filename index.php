465a4e16b7cde0e0f0e803323d397d5719bf7f08
Nick Troy <nick.troy@gmail.com>
2017-09-13 22:07:37 -0400

    Inital test of randomized quotes.

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <!-- <link rel="stylesheet" href="css/dwa15-p1.css"> -->
   </head>
   <body>
-hello
+      
+      <?php
+        $quoteArray = [
+            "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.  --Terry Pratchett",
+            "Wisdom comes from experience. Experience is often a result of lack of wisdom.  --Terry Pratchett",
+            "It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.  --Terry Pratchett",
+            "Humans need fantasy to be human. To be the place where the falling angel meets the rising ape.  --Terry Pratchett"
+        ]
+      ?>
+      
+      <p id=quotes><?php echo $quoteArray[rand(0,3)]; ?></p>
+      
   </body>
 </html>
