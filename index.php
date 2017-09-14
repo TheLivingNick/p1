43cce28e7ce7cf2a9524a954f55c3e54c6a13dcb
Nick Troy <nick.troy@gmail.com>
2017-09-13 21:36:38 -0400

    Added test index.php file.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>DWA15 Project 1</title>
+    <!-- <link rel="stylesheet" href="css/dwa15-p1.css"> -->
+  </head>
+  <body>
+hello
+  </body>
+</html>
